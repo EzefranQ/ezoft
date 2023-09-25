fc4b281dcd1c89772c9f1967d8df95fff886f766
Your Name <you@example.com>
2023-09-25 12:46:47 -0400

    fedwefe

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,10 @@ module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
   ],
-  
+  generatorOpts: {
+    compact: true,
+    minified: false,
+    shouldPrintComment: comment => /^[@#]\s+sourceMappingURL=/.test(comment)
+  },
+  compact: false
 }
